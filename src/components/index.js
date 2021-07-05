b7d2b055aeededaf00bd14bec9c18f9cea5c3984
sahmwanga <dev04@bongolive.co.tz>
2021-07-05 10:55:41 +0300

    add Cards and Tabs

--- src/components/index.js
+++ src/components/index.js
@@ -1,1 +1,8 @@
-export * from './Buttons';
+import Button from './Buttons/Button';
+import Card from './cards/card';
+import CardBody from './cards/CardBody';
+import CardFooter from './cards/CardFooter';
+import CardHeader from './cards/CardHeader';
+import Tabs from './tabs/Tabs';
+
+export { Button, Card, CardFooter, CardHeader, CardBody, Tabs };
